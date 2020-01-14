806909894f913dced402b3632100718c0d3d034d
Randy Duodu <52050586+iSOLveIT@users.noreply.github.com>
2020-01-14 23:20:18 +0000

    Update contact.py

--- content/contact.py
+++ content/contact.py
@@ -14,9 +14,9 @@ from datetime import datetime as dt
 
 def sendEmail(_name, _subject, _email, _body):
     # SEND EMAIL
-    _recipient = 'isolveitgroup@gmail.com'
-    msg = Message(_subject, sender=('COUNSELLING & GENDER UNIT', 'isolveitgroup@gmail.com'), recipients=[_recipient])
-    assert msg.sender == "COUNSELLING & GENDER UNIT <isolveitgroup@gmail.com>"
+    _recipient = 'example@gmail.com'
+    msg = Message(_subject, sender=('COUNSELLING & GENDER UNIT', 'example@gmail.com'), recipients=[_recipient])
+    assert msg.sender == "COUNSELLING & GENDER UNIT <example@gmail.com>"
     msg.body = f'''{_body}
 
 
@@ -31,8 +31,8 @@ Date Sent:  {dt.now().strftime('%B %d, %Y, %H:%M ') + 'GMT'}
 def replyMessage(_email, _senderName):
     # REPLY EMAIL
     _subj = 'Message Received'
-    mesg = Message(_subj, sender=('COUNSELLING & GENDER UNIT', 'isolveitgroup@gmail.com'), recipients=[_email])
-    assert mesg.sender == "COUNSELLING & GENDER UNIT <isolveitgroup@gmail.com>"
+    mesg = Message(_subj, sender=('COUNSELLING & GENDER UNIT', 'example@gmail.com'), recipients=[_email])
+    assert mesg.sender == "COUNSELLING & GENDER UNIT <example@gmail.com>"
     mesg.body = f'''Hello {_senderName},
 The message you sent to the COUNSELLING & GENDER UNIT has been received. The unit will contact you within 24 hours.
 Thank you.
@@ -53,10 +53,10 @@ def reset_password_link(user, email, username):
     Edate = dt.now()
     token = TokenGEN.get_reset_token(user_id=str(user_id))
     subj = 'Password Reset Requested'
-    msg = Message(subj, sender=('COUNSELLING & GENDER UNIT', 'isolveitgroup@gmail.com'), recipients=[email_address])
-    assert msg.sender == "COUNSELLING & GENDER UNIT <isolveitgroup@gmail.com>"
+    msg = Message(subj, sender=('COUNSELLING & GENDER UNIT', 'example@gmail.com'), recipients=[email_address])
+    assert msg.sender == "COUNSELLING & GENDER UNIT <example@gmail.com>"
     
-    msg.body = "<h2 style='text-align: center;'>Password Reset Instructions</h2> <br> <br> A password reset event have been requested by you on %s (GMT). <br><br> Please note that the password reset window is limited to one hour. If you do not reset your password within one hour, you will need to submit a new request. <br><br> To complete the password reset process, visit the following link: <br> <a href='https://ecounselling-upsa-478a1a.netlify.com/reset_password?token=%s'>Click here to reset password</a> <br><br> <b>Don't recognize this activity?</b> <br> Ignore this email and no changes will be made to your account.<br><br> The Counselling and Gender Unit" %(Edate.strftime('%A, %B %d, %Y %I:%M %p'), token)
+    msg.body = "<h2 style='text-align: center;'>Password Reset Instructions</h2> <br> <br> A password reset event have been requested by you on %s (GMT). <br><br> Please note that the password reset window is limited to one hour. If you do not reset your password within one hour, you will need to submit a new request. <br><br> To complete the password reset process, visit the following link: <br> <a href='http://127.0.0.1:4000/reset_password?token=%s'>Click here to reset password</a> <br><br> <b>Don't recognize this activity?</b> <br> Ignore this email and no changes will be made to your account.<br><br> The Counselling and Gender Unit" %(Edate.strftime('%A, %B %d, %Y %I:%M %p'), token)
     msg.html = msg.body
     
     mail.send(msg)
